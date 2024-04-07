8c4d454383020ab0e6d511ca6b82caf4916a9d30
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-04-07 14:17:25 +0100

    rebuild: build diff archs

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 build:
-	go build -o RELEASE/osmonitor main.go
+	GOOS=linux;GOARCH=arm64;go build -o RELEASE/arm64/osmonitor main.go
+	GOOS=linux;GOARCH=x86_64;go build -o RELEASE/x86_64/osmonitorx86 main.go
 	./copy-service-file.sh
 	cp install.sh RELEASE/install.sh
 
--- RELEASE/install.sh
+++ RELEASE/install.sh
@@ -3,7 +3,7 @@
 curl https://github.com/lampguard/osmonitor/archive/refs/tags/latest.tar.gz -L -o osmonitor.tar.gz
 
 tar -xvf osmonitor.tar.gz
-cp osmonitor-latest/RELEASE/osmonitor /usr/local/osmonitor
+cp osmonitor-latest/RELEASE/$(uname -m)/osmonitor /usr/local/bin/osmonitor
 
 useradd -r -s /bin/bash lampguard
 
