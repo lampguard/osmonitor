1c891ac1b818f8663ecce5e15f1fdad15b475b22
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-04-07 13:52:04 +0100

    rebuild: install script

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 curl https://github.com/lampguard/osmonitor/archive/refs/tags/latest.tar.gz -L -o osmonitor.tar.gz
 
-tar -xvf osmonitor.tar.gz -C osmonitor
-cp osmonitor/RELEASE/osmonitor /usr/local/osmonitor
+tar -xvf osmonitor.tar.gz
+cp osmonitor-latest/RELEASE/osmonitor /usr/local/osmonitor
 
 useradd -r -s /bin/bash lampguard
 
-cp osmonitor/RELEASE/osmonitor.service /lib/systemd/system/osmonitor.service
+cp osmonitor-latest/RELEASE/osmonitor.service /lib/systemd/system/osmonitor.service
