aebfad7da28603920339ca0f0a88f85770317e03
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-04-07 13:48:35 +0100

    rebuild: install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl https://github.com/lampguard/osmonitor/archive/refs/tags/latest.tar.gz -o osmonitor.tar.gz
+curl https://github.com/lampguard/osmonitor/archive/refs/tags/latest.tar.gz -L -o osmonitor.tar.gz
 
 tar -xvf osmonitor.tar.gz
 cp osmonitor/RELEASE/osmonitor /usr/local/osmonitor
